a3e06b290d91aa5589235af7db65300029a42cbd
Tiemani <116328962+Tiemani@users.noreply.github.com>
2024-12-23 20:07:38 +0100

    Add files via upload

---
+++ hellodjango/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'hellodjango.views.home', name='home'),
+    # url(r'^hellodjango/', include('hellodjango.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    path('admin/', admin.site.urls),  # Correct way to include the admin site
+
+]
